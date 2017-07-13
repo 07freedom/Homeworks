3db6889b5e2e7aa527be9f812619e7c1ec7c8e51
Ethan <tth379425773@126.com>
2017-07-13 18:50:12 +0800

    speeded up

--- Answers/Goldbach.py
+++ Answers/Goldbach.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
 import math
 import datetime
 
+
 # 法1：不删除偶数  
-## 结果：3 923453   时间：0:01:42.145881
+# # 结果：3 923453   时间：0:01:42.145881
 starttime_1 = datetime.datetime.now()
 def Goldbach_1(num):
     prime_num = list(filter(lambda x:not [x % j for j in range(2,int(math.sqrt(x))+1) if x % j == 0],range(2,num+1)))
@@ -13,6 +16,7 @@ def Goldbach_1(num):
 Goldbach_1(923456)
 endtime_1 = datetime.datetime.now()
 print(endtime_1 - starttime_1)
+
 
 # 法2：删除偶数
 # 结果：3 923453 时间：0:00:51.665401
@@ -25,6 +29,8 @@ def Goldbach_2(num):
         if num - i in prime_num:
             print(i,num-i)
             break
-Goldbach_2(923456)
+Goldbach_2(100000)
 endtime_2 = datetime.datetime.now()
 print(endtime_2 - starttime_2)
+
+
