1aa9171bc51476943cb4d175982b490c822bbd85
Ethan <tth379425773@126.com>
2017-07-13 18:34:04 +0800

    Speeded up

--- Answers/Goldbach.py
+++ Answers/Goldbach.py
@@ -1,9 +1,30 @@
-def Goldbach(num):
-    lists = [x for x in range(2, num) if not [y for y in range(2, x) if x % y == 0]]
-    for num1 in lists:
-        num2=num-num1
-        if num2 in lists:
-            return (num1,num2)
+import math
+import datetime
+
+# 法1：不删除偶数  
+## 结果：3 923453   时间：0:01:42.145881
+starttime_1 = datetime.datetime.now()
+def Goldbach_1(num):
+    prime_num = list(filter(lambda x:not [x % j for j in range(2,int(math.sqrt(x))+1) if x % j == 0],range(2,num+1)))
+    for i in prime_num:
+        if num - i in prime_num:
+            print(i,num-i)
             break
-			
-print (Goldbach(20))
+Goldbach_1(923456)
+endtime_1 = datetime.datetime.now()
+print(endtime_1 - starttime_1)
+
+# 法2：删除偶数
+# 结果：3 923453 时间：0:00:51.665401
+starttime_2 = datetime.datetime.now()
+def Goldbach_2(num):
+    num_range = list(range(3,num+1,2))
+    num_range.insert(0,2)
+    prime_num = list(filter(lambda x:not [x % j for j in range(2,int(math.sqrt(x))+1) if x % j == 0],num_range))
+    for i in prime_num:
+        if num - i in prime_num:
+            print(i,num-i)
+            break
+Goldbach_2(923456)
+endtime_2 = datetime.datetime.now()
+print(endtime_2 - starttime_2)
