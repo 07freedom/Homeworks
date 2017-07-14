53af0a3a7cc240a01d98f18221c62473cd142b4e
Ethan <tth379425773@126.com>
2017-07-14 11:27:36 +0800

    1.1

--- Snake.py
+++ Snake.py
@@ -5,17 +5,17 @@ def snake(N):
 		M.append([0]*N)
 	base = 0
 	for i in range(N//2):
-		for j in range(N-1-2*i):
-			M[i][j+i] = j+1+base
-			M[j+i][-1-i] = j+(N-1-2*i)+1+base
-			M[-1-i][-j-1-i] = j+2*(N-1-2*i)+1+base
-			M[-j-1-i][i] = j+3*(N-1-2*i)+1+base
+		for j in range(N-2*i-1):
+			M[i][i+j] = j+base+1
+			M[i+j][-i-1] = (N-1-2*i)+j+base+1
+			M[-i-1][-i-j-1] = 2*(N-2*i-1)+j+base+1
+			M[-i-j-1][i] = 3*(N-1-2*i)+j+1+base
 		base = base + 4*(N-1)
 	if N % 2 == 1:
 		M[N//2][N//2] = N*N
 	return M
 
-N = 7
+N = 5
 M = snake(N)
 for i in range(N):
 	print (M[i])
