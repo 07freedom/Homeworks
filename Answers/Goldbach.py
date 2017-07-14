91015d1657f5dfe362ea7205d2383f4948aa1e6b
Ethan <tth379425773@126.com>
2017-07-14 08:48:39 +0800

    2.0

--- Answers/Goldbach.py
+++ Answers/Goldbach.py
@@ -6,20 +6,20 @@ import datetime
 
 # 法1：不删除偶数  
 # # 结果：3 923453   时间：0:01:42.145881
-starttime_1 = datetime.datetime.now()
-def Goldbach_1(num):
-    prime_num = list(filter(lambda x:not [x % j for j in range(2,int(math.sqrt(x))+1) if x % j == 0],range(2,num+1)))
-    for i in prime_num:
-        if num - i in prime_num:
-            print(i,num-i)
-            break
-Goldbach_1(923456)
-endtime_1 = datetime.datetime.now()
-print(endtime_1 - starttime_1)
+# starttime_1 = datetime.datetime.now()
+# def Goldbach_1(num):
+    # prime_num = list(filter(lambda x:not [x % j for j in range(2,int(math.sqrt(x))+1) if x % j == 0],range(2,num+1)))
+    # for i in prime_num:
+        # if num - i in prime_num:
+            # print(i,num-i)
+            # break
+# Goldbach_1(923456)
+# endtime_1 = datetime.datetime.now()
+# print(endtime_1 - starttime_1)
 
 
 # 法2：删除偶数
-# 结果：3 923453 时间：0:00:51.665401
+# 结果：3 923453 时间：0:00:30
 starttime_2 = datetime.datetime.now()
 def Goldbach_2(num):
     num_range = list(range(3,num+1,2))
@@ -29,7 +29,7 @@ def Goldbach_2(num):
         if num - i in prime_num:
             print(i,num-i)
             break
-Goldbach_2(100000)
+Goldbach_2(923456)
 endtime_2 = datetime.datetime.now()
 print(endtime_2 - starttime_2)
 
--- Goldbach.py
+++ Goldbach.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
+import math
 import datetime
 
+# Input：923456
+# 结果：3 923453 时间：0:00:6.31
 starttime = datetime.datetime.now()
 def Goldbach(n):
 	primes = []
 	for num in range(3,n,2):
 	# 遍历寻找所有质数 排除所有偶数（加速1）
 		primes.append(num)
-		for i in range(3, (num//3)+1): # 只验证前1/3质数（加速2）
+		for i in range(3, int(math.sqrt(num))+1): # 只验证小于开方的质数（加速2）
 			if num % i == 0:
 				primes.pop()
-				break		
+				break		# 好像提前break又加速不少（加速3）
 	for prime in primes:
 		if n-prime in primes:
 			return ([prime, n-prime])
 	
-print (Goldbach(100000))
+print (Goldbach(923456))
 endtime = datetime.datetime.now()
 print (endtime - starttime)
